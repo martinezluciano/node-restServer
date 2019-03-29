1b9172ecfef4277957d998115abad6fd17e471b5
Luciano Martinez <luciano.martinez@globant.com>
2019-03-29 15:30:02 -0300

    test

--- server/config/config.js
+++ server/config/config.js
@@ -4,10 +4,10 @@ process.env.node_env = process.env.node_env || "dev";
 
 let URLDB;
 
-if (process.env.node_env === "dev") {
-    URLDB = "mongodb://localhost:27017/coffee";
-} else {
-    URLDB = process.env.MONGO_URI;
-}
+// if (process.env.node_env === "dev") {
+//     URLDB = "mongodb://localhost:27017/coffee";
+// } else {
+URLDB = process.env.MONGO_URI;
+// }
 process.env.URLDB = URLDB;
 module.exports = port;
