130e063ac9a0f9104893b66e2a0eaa8c6f52bc74
Luciano Martinez <luciano.martinez@globant.com>
2019-03-29 11:54:03 -0300

    modify config pass

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,12 @@
 const port = process.env.PORT || 3000;
-
+console.log(port);
 process.env.node_env = process.env.node_env || "dev";
 
 let URLDB;
-if (process.env.node_env === "dev") {
+if ((process.env.node_env = !"dev")) {
     URLDB = "mongodb://localhost:27017/coffee";
 } else {
-    URLDB = "mongodb+srv://merten:Guada2452+@cluster0-oirh4.mongodb.net/test?retryWrites=true";
+    URLDB = "mongodb+srv://merten:Guada2452@cluster0-oirh4.mongodb.net/test?retryWrites=true";
 }
 
 process.env.URLDB = URLDB;
